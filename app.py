a9b568ff40378c85f4713ae867d01d8c66959373
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-18 23:54:12 -0400

    request method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,6 +19,11 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST', 'GET'] )
 def index():
+    if request.method == 'POST':
+        """pass"""
+    else:
+        return render_template('index.html')
+
     return render_template('index.html')
 
 if __name__ == "__main__":
