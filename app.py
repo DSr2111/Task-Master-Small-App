0044586376a4270a8e3011405355783eb6ded5a0
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-20 17:22:32 -0400

    delete route

--- app.py
+++ app.py
@@ -33,6 +33,16 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try: 
+         db.session.delete(id)
+         db.session.commit()
+         return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
     
 
 if __name__ == "__main__":
